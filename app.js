1222f0fad100196d747eca8ce35441b8735b3385
shiracheifetz <scheifetz25@wits.edu>
2026-02-17 21:26:57 -0500

    totalsByCategory function

--- app.js
+++ app.js
@@ -55,7 +55,42 @@ function daySpentMost(expensesArr) {
 
 /* 5) Category breakdown (totals per category) */
 function totalsByCategory(expensesArr) {
-    return "";
+    let food = 0, transport = 0, entertainment = 0, rent = 0, school = 0, 
+        shopping = 0, subscription = 0, misc = 0, groceries = 0;
+    for (const expense of expensesArr) {
+        switch (expense.category) {
+            case "food":
+                food += expense.amount;
+                break;
+            case "transport":
+                transport += expense.amount;
+                break;
+            case "entertainment":
+                entertainment += expense.amount;
+                break;
+            case "rent":
+                rent += expense.amount;
+                break;
+            case "school":
+                school += expense.amount;
+                break;
+            case "shopping":
+                shopping += expense.amount;
+                break;
+            case "subscription":
+                subscription += expense.amount;
+                break;
+            case "misc":
+                misc += expense.amount;
+                break;
+            case "groceries":
+                groceries += expense.amount;
+                break;
+        }
+    }
+    return `You spend $${food} on food, $${transport} on transport, $${entertainment} on entertainment, ` + 
+    `$${rent} on rent, $${school} on school, $${shopping} on shopping, $${subscription} on subscriptions, ` +
+    `$${groceries} on groceries, and $${misc} on miscellaneous.`;
 }
 
 /* 6) Purchases per day (counts per day) */
